2bb8de2872639893f55b9439ea6fc2640c575e99
hao <356614679@qq.com>
2022-11-22 23:07:59 +0800

    learn Optional.ofNullable() orElse() orElseGet() Boolean.parseBoolean

--- src/test/java/com/codinggrass/learn/junit5/FirstSample.java
+++ src/test/java/com/codinggrass/learn/junit5/FirstSample.java
@@ -5,15 +5,18 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.platform.commons.util.StringUtils;
 import sun.util.calendar.BaseCalendar;
 
 import java.io.PrintStream;
 import java.sql.Date;
 import java.time.Instant;
 import java.util.Enumeration;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.Properties;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -34,16 +37,56 @@ class FirstSample {
 
     @Test
     void testPropertyUse() {
-        Properties properties = new Properties();
-        properties.setProperty("name", "hao");
-        properties.setProperty("date", "11-21");
-        properties.setProperty("purpose", "code");
+        Properties properties = getOneProperties();
 
         Enumeration<?> enumeration = properties.propertyNames();
         Object element = enumeration.nextElement();
         assertEquals(properties.getProperty((String) element), "code");
 
         assertEquals(properties.getOrDefault("date", Date.from(Instant.now()).toString()),"11-21");
+    }
+
+    private Properties getOneProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("name", "hao");
+        properties.setProperty("date", "11-21");
+        properties.setProperty("purpose", "code");
+        return properties;
+    }
+
+    @Test
+    void testOptional() {
+        Properties oneProperties = getOneProperties();
+        assertNotNull(Optional.ofNullable(oneProperties.getProperty("name")));
+        assertNull(null);
+        Optional<Object> empty = Optional.empty();
+        log.info(Optional.ofNullable(oneProperties.getProperty("name")).toString());
+        log.info(Optional.ofNullable(oneProperties.getProperty("name")).map(String::toUpperCase).toString());
+        log.info(Optional.ofNullable(oneProperties.getProperty("time")).map(String::toUpperCase).toString());
+        log.info(Optional.ofNullable(oneProperties.getProperty("name")).get());
+        log.info("{}",Optional.ofNullable(oneProperties.getProperty("time")).orElse(null));
+        assertThrows(NoSuchElementException.class,()->Optional.ofNullable(oneProperties.getProperty("time")).get());;
+    }
+
+    @Test
+    void testBoolean() {
+        /*parseBoolean 不为“true”都为false*/
+        assertFalse(Boolean.parseBoolean("false"));
+        assertTrue(Boolean.parseBoolean("true"));
+        assertTrue(Boolean.parseBoolean("TRUE"));
+        assertTrue(Boolean.parseBoolean("TRuE"));
+        assertFalse(Boolean.parseBoolean("0"));
+        assertFalse(Boolean.parseBoolean("1"));
+        assertFalse(Boolean.parseBoolean("123"));
+        assertFalse(Boolean.parseBoolean("abc"));
+    }
+
+    @Test
+    void testOptionalElseGet() {
+        Optional<Object> optionalBool = Optional.empty();
+        assertNotNull(optionalBool.orElseGet(()-> "A"));
+        Object a = Optional.ofNullable(null).orElse("a");
+        log.info("{} {}",a.getClass(),a);
     }
 
 
